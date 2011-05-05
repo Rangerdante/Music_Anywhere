8c33a6c3f8673714c6c4e6a65a0013e3622a7ef2
rangerdante <w.garland.long@gmail.com>
2011-05-05 18:48:07 -0400

    blah

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
 		self.current_user = user
 	end
 	
-	def current_user 
+	def current_user
 		@current_user ||= user_from_remember_token
 	end
 	
@@ -16,6 +16,10 @@ module SessionsHelper
 	def sign_out
 		cookies.delete(:remember_token)
 		self.current_user = nil
+	end
+	
+	def current_user=(user)
+		@current_user = user
 	end
 	
 	private
