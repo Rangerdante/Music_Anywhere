360dbabf4904ff58a428223d759ebb7a352bfea9
rangerdante <w.garland.long@gmail.com>
2011-05-05 15:10:17 -0400

    Added sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
 	resources :users
 	
-	match '/signup',	:to => 'users#new'
+	match '/signin',	:to => 'users#show'
+	match '/new', 		:to => 'users#new'
 	
 	match '/contact', 	:to => 'pages#contact'
 	match '/about', 	:to => 'pages#about'
